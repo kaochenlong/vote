05ee923dfacabc354c5dfd591a36b8f8830d38c6
Eddie Kao <eddie@5xruby.tw>
2019-07-01 17:32:14 +0800

    add candidate successfully

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,9 +4,22 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      flash[:notice] = "Candidate created!"
+      redirect_to '/candidates'
+    else
+      # NG
+    end
   end
 
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
 end
