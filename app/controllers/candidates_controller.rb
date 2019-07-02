9bda5fc8d7c87bd71f993d2d856c557d3cc6aebd
Eddie Kao <eddie@5xruby.tw>
2019-07-02 18:29:04 +0800

    add delete function

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -23,6 +23,29 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+
+    if @candidate.update(candidate_params)
+      flash[:notice] = "Candidate updated!"
+      redirect_to '/candidates'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy
+
+    flash[:notice] = "Candidate deleted!"
+    redirect_to '/candidates'
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
