54b2492789d68079066300c14d3c970bc8fa1d0c
Eddie Kao <eddie@5xruby.tw>
2019-07-04 17:55:44 +0800

    refactor Candidate controller and view

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,11 +1,12 @@
 class CandidatesController < ApplicationController
 
+  before_action :find_candidate, only: [:show, :edit, :update, :destroy, :vote]
+
   def index
     @candidates = Candidate.all
   end
 
   def show
-   @candidate = Candidate.find_by(id: params[:id])
   end
 
   def new
@@ -16,48 +17,40 @@ class CandidatesController < ApplicationController
     @candidate = Candidate.new(candidate_params)
 
     if @candidate.save
-      flash[:notice] = "Candidate created!"
-      redirect_to '/candidates'
+      redirect_to '/candidates', notice: 'Candidate created!'
     else
       render :new
     end
   end
 
   def edit
-    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def update
-    @candidate = Candidate.find_by(id: params[:id])
-
     if @candidate.update(candidate_params)
-      flash[:notice] = "Candidate updated!"
-      redirect_to '/candidates'
+      redirect_to '/candidates', notice: 'Candidate updated!'
     else
       render :edit
     end
   end
 
   def destroy
-    @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy
-
-    flash[:notice] = "Candidate deleted!"
-    redirect_to '/candidates'
+    redirect_to '/candidates', notice: 'Candidate deleted!'
   end
 
   def vote
-    @candidate = Candidate.find_by(id: params[:id])
-
     # VoteLog.create(candidate: @candidate, ip_address: request.remote_ip)
     @candidate.vote_logs.create(ip_address: request.remote_ip)
-
-    flash[:notice] = "Voted!"
-    redirect_to '/candidates'
+    redirect_to '/candidates', notice: 'Voted!'
   end
 
   private
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
   end
 end
